a0068a32a50222330c31a1e4b5e0d8d6bb17c365
Andre Noberto <andreno.souza@gmail.com>
2017-10-26 21:11:44 -0200

    Added main form

--- JavaPetri/src/com/trafficlights/TrafficLights.java
+++ JavaPetri/src/com/trafficlights/TrafficLights.java
@@ -1,4 +1,16 @@
 package com.trafficlights;
 
+import javax.swing.*;
+
 public class TrafficLights {
+    private JButton button1;
+    private JPanel MainPanel;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Colored Petri Net - Traffic Lights");
+        jFrame.setContentPane(new TrafficLights().MainPanel);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
 }
