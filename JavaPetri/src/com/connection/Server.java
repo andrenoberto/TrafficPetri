cc1035457fa66a758cdf274811148c135708b4e0
Andre Noberto <andreno.souza@gmail.com>
2017-11-01 12:13:54 -0200

    Making server connection class

---
+++ JavaPetri/src/com/connection/Server.java
@@ -0,0 +1,12 @@
+package com.connection;
+
+import com.cpn.CPNTools;
+
+public class Server {
+    public CPNTools cpnTools;
+    public CPNTools cpnField;
+    private String hostName;
+    private int inputPort;
+    private int outputPort;
+    public String message;
+}
