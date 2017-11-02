d60c558e6732285368a315c2b5969bfbbb7a7b6b
Andre Noberto <andreno.souza@gmail.com>
2017-11-02 11:48:02 -0200

    Simplified sample

--- JavaPetri/src/com/connection/Sample.java
+++ JavaPetri/src/com/connection/Sample.java
@@ -2,17 +2,37 @@ package com.connection;
 
 import com.cpn.CPNTools;
 import com.messagehelper.Decode;
-import com.messagehelper.Encode;
-import com.sun.org.apache.xpath.internal.SourceTree;
 
 import java.io.IOException;
 import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Sample {
-    public static void main(String[] args) {
+    public CPNTools cpnTools = new CPNTools();
+    public boolean connectedToCPN = false;
+    public int port = 9000;
+
+    public void init() throws Exception {
+        System.out.println("Waiting for a new connection...");
+        try {
+            this.cpnTools.accept(this.port);
+            this.connectedToCPN = true;
+            System.out.println("Connection found...\nEstablishing communication...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (this.connectedToCPN) {
+            String result = "";
+            try {
+                result = Decode.decodeString(this.cpnTools.receive());
+                System.out.println("Message: " + result);
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         /*
         This sample only prints the results sent from CPNTools.
         If you actually want to see the graphical interface, you need
@@ -20,22 +40,9 @@ public class Sample {
         This sample is for debug purposes only, it will not affect
         how this application works.
          */
-        int port = 9000;
-        boolean connectedToCPN = false;
-        CPNTools cpnTools = new CPNTools();
-        try {
-            cpnTools.accept(port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String result = "";
-        while (true) {
-            try {
-                result = Decode.decodeString(cpnTools.receive());
-                System.out.println(result);
-            } catch (SocketException e) {
-                e.printStackTrace();
-            }
-        }
+        Sample sample = new Sample();
+        sample.init();
     }
 }
+
+
