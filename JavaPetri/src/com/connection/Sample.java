72e95147927ebb084ff163d20ff07f2d83c70c7d
Andre Noberto <andreno.souza@gmail.com>
2017-11-02 10:20:45 -0200

    Added comments explaning the purpose of sample

--- JavaPetri/src/com/connection/Sample.java
+++ JavaPetri/src/com/connection/Sample.java
@@ -13,6 +13,13 @@ import java.util.logging.Logger;
 
 public class Sample {
     public static void main(String[] args) {
+        /*
+        This sample only prints the results sent from CPNTools.
+        If you actually want to see the graphical interface, you need
+        to compile and execute the binaries.
+        This sample is for debug purposes only, it will not affect
+        how this application works.
+         */
         int port = 9000;
         boolean connectedToCPN = false;
         CPNTools cpnTools = new CPNTools();
