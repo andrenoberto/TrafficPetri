a9adf9bdca2f9283e677e8f38a3b294af0cc411f
Andre Noberto <andreno.souza@gmail.com>
2017-11-03 13:18:19 -0200

    Added thread and fixed infite looping

--- JavaPetri/src/com/main/Main.java
+++ JavaPetri/src/com/main/Main.java
@@ -5,29 +5,37 @@ import com.trafficlights.TrafficLights;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        TrafficLights trafficLights = new TrafficLights(false);
-        trafficLights.setContentPane(trafficLights.getMainPanel());
-        trafficLights.pack();
-        trafficLights.setVisible(true);
-        trafficLights.get_fOpenClose().setEnabled(false);
-        /*
-        CPNTools communication stuff
-         */
-        trafficLights.setTrafficLightsToDefault();
-        while (true) {
-            try {
-                trafficLights.getCPNToolsObject().accept(9000);
-                trafficLights.setConnectedToCPN(true);
-                trafficLights.get_fOpenClose().setEnabled(true);
-                trafficLights.setStatusMessageLabel("connected to CPNTools.");
-            } catch (IOException e) {
-                trafficLights.setStatusMessageLabel("could not connect/disconnect CPNTools to this application.");
-            }
+    public static class TrafficLightsRunnable implements Runnable {
+        @Override
+        public void run() {
+            TrafficLights trafficLights = new TrafficLights(false);
+            trafficLights.setContentPane(trafficLights.getMainPanel());
+            trafficLights.pack();
+            trafficLights.setVisible(true);
+            trafficLights.get_fOpenClose().setEnabled(false);
+            /*
+                CPNTools communication stuff
+             */
+            trafficLights.setTrafficLightsToDefault();
+            while (trafficLights.isApplicationRunning()) {
+                try {
+                    trafficLights.getCPNToolsObject().accept(9000);
+                    trafficLights.setConnectedToCPN(true);
+                    trafficLights.get_fOpenClose().setEnabled(true);
+                    trafficLights.setStatusMessageLabel("connected to CPNTools.");
+                } catch (IOException e) {
+                    trafficLights.setStatusMessageLabel("could not connect/disconnect CPNTools to this application.");
+                }
 
-            while (trafficLights.getConnectedToCPN()) {
-                trafficLights.communicateWithCPNTools();
+                while (trafficLights.getConnectedToCPN()) {
+                    trafficLights.communicateWithCPNTools();
+                }
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new TrafficLightsRunnable());
+        thread.start();
     }
 }
